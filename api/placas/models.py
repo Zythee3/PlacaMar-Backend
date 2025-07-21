4223cdc8d964373565e61fcdce7da367d5c48d4a
Lucasrc22 <lucasrios2210@gmail.com>
2025-07-21 08:30:23 -0300

    att models das placas

--- api/placas/models.py
+++ api/placas/models.py
@@ -1,15 +1,17 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 class Placa(models.Model):
     nome_placa = models.CharField(max_length=255)
-    subzona = models.CharField(max_length=50)
-    localidade = models.CharField(max_length=100)
-    embarcacoes = models.IntegerField()
-    usuarios = models.IntegerField()
-    cor = models.CharField(max_length=7)
-    descricao = models.TextField()
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    localidade = models.CharField(max_length=255)
+    zona = models.CharField(max_length=255)
+    atividades_permitidas = models.TextField(blank=True, null=True)
+    qtd_placas = models.IntegerField(blank=True, null=True)
+    descricao = models.TextField(blank=True, null=True)
+    num_embarcacoes = models.IntegerField(blank=True, null=True)
+    max_pessoas_catamara = models.IntegerField(blank=True, null=True)
+    max_pessoas_miudas = models.IntegerField(blank=True, null=True)
+    # Campo geográfico PointField para latitude/longitude
+    location = models.PointField(geography=True, blank=True, null=True)
 
     def __str__(self):
         return self.nome_placa
--- api/placas/urls.py
+++ api/placas/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import lista_placas
 
 urlpatterns = [
-    path('', lista_placas, name='lista_placas'),
+    path('placas/', lista_placas, name='lista_placas'),
 ]
