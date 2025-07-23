c7a1828631659f1e38b430eba7b385a0eff1c0d2
rimaud <rimauddjido@gmail.com>
2025-07-23 20:03:49 -0300

    Atualização

--- api/placas/models.py
+++ api/placas/models.py
@@ -1,17 +1,1 @@
 from django.contrib.gis.db import models
-
-class Placa(models.Model):
-    nome_placa = models.CharField(max_length=255)
-    localidade = models.CharField(max_length=255)
-    zona = models.CharField(max_length=255)
-    atividades_permitidas = models.TextField(blank=True, null=True)
-    qtd_placas = models.IntegerField(blank=True, null=True)
-    descricao = models.TextField(blank=True, null=True)
-    num_embarcacoes = models.IntegerField(blank=True, null=True)
-    max_pessoas_catamara = models.IntegerField(blank=True, null=True)
-    max_pessoas_miudas = models.IntegerField(blank=True, null=True)
-    # Campo geográfico PointField para latitude/longitude
-    location = models.PointField(geography=True, blank=True, null=True)
-
-    def __str__(self):
-        return self.nome_placa
