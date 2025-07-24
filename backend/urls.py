8f8e66040a5c6528d8886572287e231ddd6a588c
rimaud <rimauddjido@gmail.com>
2025-07-23 23:26:20 -0300

    atualização

--- backend/urls.py
+++ backend/urls.py
@@ -1,13 +1,9 @@
-from django.http import HttpResponse
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-def home(request):
-    return HttpResponse("API PlacaMar Backend funcionando!")
-
 urlpatterns = [
-    path('', home),  # Rota raiz simples
+    path('', admin.site.urls),  # Rota raiz para o admin do Django
     path('admin/', admin.site.urls),
     path('api/admin/', include('core_admin.urls')),
     path('api/usuarios/', include('usuarios.urls')),
